dd5c6b2840756b791406bba468ac7180bb467844
Kristian Veter <kristian.v.dev@gmail.com>
2025-08-12 19:19:23 +0300

    chore: update company email from thematchengine to paperfly domain

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,7 +8,7 @@ export const DASHBOARD_URL = "https://dashboard.paperfly.app";
 
 // Company information
 export const COMPANY_NAME = "Paperfly";
-export const COMPANY_EMAIL = "info@thematchengine.com";
+export const COMPANY_EMAIL = "support@paperfly.app";
 export const COMPANY_WEBSITE = "https://paperfly.app/";
 
 // Social media
